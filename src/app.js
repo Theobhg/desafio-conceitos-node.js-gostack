0918a49e28ec4ac7c9edd06bcf94936c4843db68
Theobhg <assis.theo@gmail.com>
2020-06-21 22:30:55 -0300

    all methods passed/add middleware

--- src/app.js
+++ src/app.js
@@ -10,6 +10,20 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params;
+
+  const respostoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (respostoryIndex < 0 ) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  return next();
+}
+
+app.use("/repositories/:id", validateId);
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
@@ -30,16 +44,42 @@ app.post("/repositories", (request, response) => {
   return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const respostoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  const repository = {
+    ...repositories[respostoryIndex],
+    title,
+    url, 
+    techs,
+  };
+
+  repositories[respostoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+
+  const respostoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  repositories.splice(respostoryIndex, 1);
+  
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { id } = request.params;
+
+  const respostoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  repositories[respostoryIndex].likes++;
+
+  return response.json(repositories[respostoryIndex]);
 });
 
 module.exports = app;
